d847d502330d7a93de7e8cb8b633cc896de2bc7f
Juliano <julianohbl@hotmail.com>
2021-07-01 19:37:46 -0300

    Exercício FizzBuzz

---
+++ src/script.js
@@ -0,0 +1,25 @@
+// FizzBuzz
+// Divisível por 3 => 'Fizz',
+// Divisível por 5 => 'Buzz',
+// Divisível por 3 e 5 => 'FizzBuzz',
+// Se não for um número => 'Não é um número',
+// Se não for divisível nem por 3 e nem por 5 => Entrada.
+
+let resultado = fizzbuzz(3);
+console.log(resultado);
+
+function fizzbuzz(entrada) {
+  if (typeof entrada !== "number") {
+    return "Não é um número";
+  }
+  if (entrada % 3 === 0 && entrada % 5 === 0) {
+    return "FizzBuzz";
+  }
+  if (entrada % 3 === 0) {
+    return "Fizz";
+  }
+  if (entrada % 5 === 0) {
+    return "Buzz";
+  }
+  return entrada;
+}
