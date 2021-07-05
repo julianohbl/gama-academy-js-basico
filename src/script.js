868d0d74ea3e0de582801258ae7b94ef493a25f7
Juliano <julianohbl@hotmail.com>
2021-07-05 11:54:15 -0300

    Exercício To do list

--- src/script.js
+++ src/script.js
@@ -1,8 +1,38 @@
-// Convert Celsius to Fahrenheit
+const listContainer = document.querySelector("[data-lists]");
+const newListForm = document.querySelector("[data-new-list-form]");
+const newListInput = document.querySelector("[data-new-list-input]");
 
-let result = convertToFahrenheit(40);
-console.log(`O valor em Fahrenheit é ${result}`);
+let lists = [];
 
-function convertToFahrenheit(value) {
-  return value * 1.8 + 32;
+newListForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const listName = newListInput.value;
+
+  if (listName === null || listName === "") return;
+
+  const list = createList(listName);
+  newListInput.value = null;
+  lists.push(list);
+  render();
+});
+
+function createList(name) {
+  return { id: Date.now().toString(), name: name };
+}
+
+function render() {
+  clearElement(listContainer);
+
+  lists.forEach(function (list) {
+    const item = document.createElement("li");
+    item.classList.add("item");
+    item.innerText = list.name;
+    listContainer.appendChild(item);
+  });
+}
+
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
 }
